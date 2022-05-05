b88a4bfe3d1376ce9b10838b3dfdb044d592d7a1
thomas.duvinage <43036801+totordudu@users.noreply.github.com>
2022-05-05 21:54:34 +0200

    add serial reading test to main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -3,56 +3,60 @@
 #include <QApplication>
 
 #if defined(__linux__) || defined(__APPLE__)
-//#define SERIAL_PORT "/dev/ttyS0"
-#define SERIAL_PORT "/dev/cu.usbmodem143101"
+//#define SERIAL_PORT "/dev/ttyS0" "/dev/cu.usbmodem143101"
+#define SERIAL_PORT "/dev/tty"
 #endif
 
 int main(int argc, char *argv[])
 {
-    QApplication a(argc, argv);
-    StudentAnalyzer w;
-    w.show();
-    return a.exec();
+    // QApplication a(argc, argv);
+    // StudentAnalyzer w;
+    // w.show();
+    // return a.exec();
 
-    // // Serial object
-    // serialib serial;
+    // Serial object
+    serialib serial;
 
-    // // Connection to serial port
-    // char errorOpening = serial.openDevice(SERIAL_PORT, 9600);
+    // Connection to serial port
+    char errorOpening = serial.openDevice(SERIAL_PORT, 115200);
 
-    // // If connection fails, return the error code otherwise, display a success message
-    // if (errorOpening != 1)
-    //     return errorOpening;
+    // If connection fails, return the error code otherwise, display a success message
+    if (errorOpening != 1)
+        return errorOpening;
 
-    // printf("Successful connection to %s\n", SERIAL_PORT);
+    printf("Successful connection to %s\n", SERIAL_PORT);
 
-    // // Set DTR
-    // serial.DTR(true);
-    // // Clear RTS
-    // serial.RTS(false);
+    // Set DTR
+    serial.DTR(true);
+    // Clear RTS
+    serial.RTS(false);
 
-    // char *reading_string = (char *)malloc(100 * sizeof(char));
-    // char finalchar = '\n';
+    char *reading_string;
+    char finalchar = '\n';
 
-    // // Loop forever
-    // while (1)
-    // {
-    //     // Read and display the status of each pin
-    //     // DTR should be 1
-    //     // RTS should be 0
-    //     printf("4-DTR=%d\t", serial.isDTR());
-    //     printf("7-RTS=%d\t", serial.isRTS());
+    // Loop forever
+    while (1)
+    {
+        // Read and display the status of each pin
+        // DTR should be 1
+        // RTS should be 0
+        printf("4-DTR=%d\t", serial.isDTR());
+        printf("7-RTS=%d\t", serial.isRTS());
 
-    //     printf("1-DCD=%d\t", serial.isDCD());
-    //     printf("8-CTS=%d\t", serial.isCTS());
-    //     printf("6-DSR=%d\t", serial.isDSR());
-    //     printf("9-RING=%d\n", serial.isRI());
+        printf("1-DCD=%d\t", serial.isDCD());
+        printf("8-CTS=%d\t", serial.isCTS());
+        printf("6-DSR=%d\t", serial.isDSR());
+        printf("9-RING=%d\n", serial.isRI());
 
-    //     serial.readString(reading_string, finalchar, 100, 100);
+        // serial.readString(reading_string, finalchar, 100, 100);
 
-    //     printf("message : %s\n", reading_string);
-    // }
+        // printf("message : %s\n", reading_string);
 
-    // // Close the serial device
-    // serial.closeDevice();
+        sleep(1);
+    }
+
+    // Close the serial device
+    serial.closeDevice();
+
+    return 0;
 }
